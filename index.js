309d9928bff073252e5aecf4c21e650daf770dc1
Kevin DeLand <kevin.deland@gmail.com>
2014-09-09 11:28:17 -0400

    blue auth without passport

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
  * New node file
  */
 var W3AuthStrategy = require('./lib/passport-w3auth')
-,	permissions = require('./lib/permissions');
+,	permissions = require('./lib/permissions')
+,	BlueAuth = require('./lib/blue-auth');
 
 exports.W3AuthStrategy = W3AuthStrategy;
 exports.permissions = permissions;
+exports.BlueAuth = BlueAuth;
